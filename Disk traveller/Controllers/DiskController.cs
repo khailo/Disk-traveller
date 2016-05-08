2ddffe353c709cf2943556a5ab16a90b2b935d56
khailo <gr.khailo@gmail.com>
2016-05-08 10:35:42 +0300

    alll

---
+++ Disk traveller/Controllers/DiskController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Disk_traveller.Models;
+
+
+namespace Disk_traveller.Controllers
+{
+    public class DiskController : ApiController
+    {
+
+        private Folderer folderer;
+
+        public DiskController()
+        {
+            folderer = new Folderer();
+        }
+
+        public IEnumerable<string> Get()
+        {
+            
+            folderer.GetDrives();
+            return folderer.drives; 
+        }
+
+        public Dictionary<string, Dictionary<string, string>> Get(string id)
+        {
+            byte[] data = Convert.FromBase64String(id);
+            string decodedString = System.Text.Encoding.UTF8.GetString(data);
+
+            id = System.Web.HttpUtility.UrlDecode(decodedString);
+            folderer.GetFolders(id);
+            return folderer.folders;
+        }  
+
+    }
+}
